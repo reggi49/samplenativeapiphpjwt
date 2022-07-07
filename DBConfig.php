afec4cf111c7629eb572f5cd3a7719ee8527252c
reggi49 <reggi49@users.noreply.github.com>
2022-07-07 16:24:54 +0700

    bpjs file

---
+++ DBConfig.php
@@ -0,0 +1,15 @@
+<?php
+ 
+//Define your host here.
+$HostName = "localhost";
+ 
+//Define your database name here.
+$DatabaseName = "dynamo";
+ 
+//Define your database username here.
+$HostUser = "user";
+ 
+//Define your database password here.
+$HostPass = "pass";
+ 
+?>
